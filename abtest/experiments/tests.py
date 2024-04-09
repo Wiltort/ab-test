57efc4864b2501a26736e85ec1dc03d5631a03ee
Wiltort <wiltort21@gmail.com>
2024-04-09 21:24:27 +0500

    лор

--- abtest/experiments/tests.py
+++ abtest/experiments/tests.py
@@ -1,31 +1,32 @@
 from django.test import TestCase, Client
+from rest_framework.test import APIRequestFactory, APIClient
 
 # Create your tests here.
 class SimpleTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.token = 'z0000'
+        self.factory = APIRequestFactory()
 
-    def DeviceTest(self):
-        request=self.client.get(path='api/experiments/', 
-                                headers={'Device-Token':self.token})
-        self.assertEqual(request.status_code, 200, msg='Запрос некорректный')
+    def test_device(self):
+        res=self.client.get(path='/api/experiments/', 
+                                headers={'Device-Token': self.token,})
+        self.assertEqual(res.status_code, 200, msg='Запрос некорректный')
+        print(self.token)
         for i in range(10):
-            r = self.client.get(path='api/experiments/', 
-                                headers={'Device-Token':self.token})
-            self.assertEqual(request.content, r.content, msg='Данные опыта изменились')
+            r = self.client.get(path='/api/experiments/', 
+                                headers={'Device-Token': self.token,})
+            self.assertEqual(res.content, r.content, msg='Данные опыта изменились')
+            print(res.request)
 
-    def DistributionTest(self):
+    def test_distribution(self):
         N=[0]*3
         for i in range(600):
             token = str(i)
-            request = self.client.get(path='api/experiments/', 
+            request = self.client.get(path='/api/experiments/', 
                                       headers={'Device-Token':token})
             self.assertEqual(request.status_code, 200, msg='Запрос некорректный')
-            for a in request.context:
-                if a["key"] == 'button_color':
-                    print('da')
-        self.assertTrue("2")
+            
 
     def tearDown(self):
         print("tearDown")
